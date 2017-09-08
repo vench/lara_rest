73d257941dbbd6bc8543f15d0f969f0cfc812fa9
Vench <nowwrit@mail.ru>
2017-09-08 09:05:59 +0300

    0907 add laravel config

--- src/RestServiceProviderLaravel.php
+++ src/RestServiceProviderLaravel.php
@@ -9,6 +9,15 @@
 namespace LpRest;
 
 use Illuminate\Support\ServiceProvider;
+use LpRest\Repositories\CommonRepositoryModelProvider;
+use LpRest\Repositories\CommonRepositoryModelProviderBase;
+use LpRest\Repositories\CommonRepositoryAccessProvider;
+use LpRest\Repositories\CommonRepositoryAccessProviderBase;
+use LpRest\Controllers\CommonResponse;
+use LpRest\Controllers\CommonResponseBase;
+use LpRest\Repositories\RepositoryProvider;
+use LpRest\Repositories\RepositoryProviderBase;
+use Route;
 
 /**
  * Class RestServiceProviderLaravel
@@ -17,5 +26,85 @@ use Illuminate\Support\ServiceProvider;
  */
 class RestServiceProviderLaravel extends ServiceProvider
 {
+    /**
+     * @var RestServiceHelper
+     */
+    private $restServiceHelper;
+
+    /**
+     *
+     */
+    public function boot()
+    {
+
+        $this->restServiceHelper = $this->app->make(RestServiceHelper::class);
+
+        $this->registerRoute();
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->app->bindIf(CommonRepositoryModelProvider::class,
+            CommonRepositoryModelProviderBase::class, true);
+
+        $this->app->bindIf(CommonRepositoryAccessProvider::class,
+            CommonRepositoryAccessProviderBase::class, true);
+
+        $this->app->bindIf(CommonResponse::class,
+            CommonResponseBase::class, true);
+
+        $this->app->bindIf(RepositoryProvider::class,
+            RepositoryProviderBase::class, true);
+
+    }
+
+    /**
+     *
+     */
+    public function registerRoute() {
+
+        $optionGroup = array_merge([
+            'prefix'        => 'rest',
+            'middleware'    => [],
+        ], $this->restServiceHelper->getRouteGroupOptions() );
+
+
+        Route::group($optionGroup, function () {
+
+            //rest api
+            Route::post('multi', '\LpRest\Controllers\CommonController@multi');
+            Route::get('{modelName}/{relations?}', '\LpRest\Controllers\CommonController@all')->where([
+                'relations' => '^[A-Za-z][A-Za-z0-9\/]+',
+                'modelName' => '^[A-Za-z][A-Za-z0-9]+',
+            ]);
+            Route::get('{modelName}/{id}/{relations?}', '\LpRest\Controllers\CommonController@one')->where([
+                'id'        => '[0-9]+',
+                'relations' => '^[A-Za-z][A-Za-z0-9\/]+',
+                'modelName' => '^[A-Za-z][A-Za-z0-9]+',
+            ]);
+            Route::delete('{modelName}/{id}', '\LpRest\Controllers\CommonController@delete')->where([
+                'id'        => '[0-9]+',
+                'modelName' => '^[A-Za-z][A-Za-z0-9]+',
+            ]);
+            Route::post('{modelName}', '\LpRest\Controllers\CommonController@create');
+            Route::put('{modelName}/{id}', '\LpRest\Controllers\CommonController@update')->where([
+                'id'        => '[0-9]+',
+                'modelName' => '^[A-Za-z][A-Za-z0-9]+',
+            ]);
+            Route::post('{modelName}/{id}/{methodName}',  '\LpRest\Controllers\CommonController@call')->where([
+                'id'        => '[0-9]+',
+                'modelName' => '^[A-Za-z][A-Za-z0-9]+',
+                'methodName' => '^[A-Za-z][A-Za-z0-9]+',
+            ]);
+        });
+    }
+
 
 }
